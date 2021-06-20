91181e2f8fb2fa193ac616a13df75f2e2809a23f
arminmalek <33123807+arminmalek@users.noreply.github.com>
2021-06-21 00:54:36 +0430

    fixed download bug

--- server.js
+++ server.js
@@ -24,43 +24,53 @@ async function startDl(body) {
   await Promise.all(
     body.imgs.map(async (element) => {
       let tmp = await downloadImage(element, body.width);
-      tmp = tmp.replace("/app", "");
-      console.log("rr", tmp);
-      lins.push(tmp);
+      //console.log("before tmp", tmp);
+      if (tmp != undefined) {
+        //console.log("OK", tmp);
+        tmp = tmp.replace("/app", "");
+        //console.log("rr", tmp);
+        lins.push(tmp);
+      }
     })
-  ).catch((err) => {});
-
-  console.log("return dl", lins);
+  );
+  //console.log("return dl", lins);
   return lins;
 }
 async function downloadImage(url, width = 900) {
   let loc;
-  const res = await axios.get(url, { responseType: "arraybuffer" });
-  await sharp(res.data)
-    .resize(width)
-    .webp()
-    .toBuffer()
-    .then((data) => {
-      loc =
-        __dirname +
-        "/" +
-        url
-          .split("/")
-          .pop()
-          .replace(".png", ".webp")
-          .replace(".PNG", ".webp")
-          .replace(".jpg", ".webp")
-          .replace(".JPG", ".webp")
-          .replace(".jpeg", ".webp")
-          .replace(".JPEG", ".webp")
-          .replace(".gif", ".webp")
-          .replace(".GIF", ".webp");
-      fs.promises.writeFile(loc, data);
-      console.log(loc);
-      return loc;
-    })
-    .catch((err) => {
-      Console.log(err);
-    });
+  try {
+    const res = await axios.get(url, { responseType: "arraybuffer" });
+    await sharp(res.data)
+      .resize(width)
+      .webp()
+      .toBuffer()
+      .then((data) => {
+        loc =
+          __dirname +
+          "/" +
+          url
+            .split("/")
+            .pop()
+            .replace(".png", ".webp")
+            .replace(".PNG", ".webp")
+            .replace(".jpg", ".webp")
+            .replace(".JPG", ".webp")
+            .replace(".jpeg", ".webp")
+            .replace(".JPEG", ".webp")
+            .replace(".gif", ".webp")
+            .replace(".GIF", ".webp");
+        fs.promises.writeFile(loc, data);
+        console.log(loc);
+        return loc;
+      })
+      .catch((err) => {
+        Console.log(err);
+      });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+    }
+  }
+  //console.log(loc, "loc");
   return loc;
 }
